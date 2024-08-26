37fb9dbd585067bd069004bab7f4dc77b771a7dc
Jhonatan ardila <jhonatan.alvarez@netuxtecnologia.com>
2024-08-26 17:09:22 -0500

    feat: change url mongo

--- src/database.js
+++ src/database.js
@@ -6,7 +6,7 @@ import mongoose  from "mongoose"
 
 
 
-mongoose.connect("mongodb://localhost:27017/companydb",{
+mongoose.connect("mongodb://Localhost/companydb",{
     useNewUrlParser: true,
     useUniFiedTopology: true,
     
