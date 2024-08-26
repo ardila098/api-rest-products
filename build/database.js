c40cf3031bbe093af496d5d49e3f181b8a56c0a0
Jhonatan ardila <jhonatan.alvarez@netuxtecnologia.com>
2024-08-26 17:09:39 -0500

    feat: change url mongo

--- build/database.js
+++ build/database.js
@@ -4,7 +4,7 @@ var _mongoose = _interopRequireDefault(require("mongoose"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
 //hago la conexion con mongo y localhost y la base de datos se va llamar companydb y le pongo un then y cash para verificar conexion
 
-_mongoose["default"].connect("mongodb://localhost:27017/companydb", {
+_mongoose["default"].connect("mongodb://Localhost/companydb", {
   useNewUrlParser: true,
   useUniFiedTopology: true
 }).then(function (db) {
