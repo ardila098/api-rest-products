e34d7d2bb1a75373ad59e271e66022022818bd88
Jhonatan ardila <jhonatan.alvarez@netuxtecnologia.com>
2024-08-30 21:11:16 -0500

    feat: change dir uploads

--- build/controllers/upload.js
+++ build/controllers/upload.js
@@ -1,11 +1,21 @@
 "use strict";
 
-// upload.js
-
 var multer = require("multer");
+var path = require('path');
+var fs = require('fs');
+
+// Obtener la ruta absoluta del directorio de uploads
+var uploadDir = path.resolve(__dirname, '..', 'public', 'uploads');
+
+// Crear el directorio si no existe
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, {
+    recursive: true
+  });
+}
 var storage = multer.diskStorage({
   destination: function destination(req, file, cb) {
-    cb(null, "../public/uploads");
+    cb(null, uploadDir);
   },
   filename: function filename(req, file, cb) {
     cb(null, "".concat(Date.now(), "-").concat(file.originalname));
@@ -18,9 +28,14 @@ var multerConfig = {
   },
   // 1MB
   fileFilter: function fileFilter(req, file, cb) {
-    // filters
-
-    cb(null, true);
+    // Filtros para tipos de archivo permitidos
+    var allowedFileTypes = /jpeg|jpg|png/;
+    var mimeType = allowedFileTypes.test(file.mimetype);
+    var extname = allowedFileTypes.test(path.extname(file.originalname));
+    if (mimeType && extname) {
+      return cb(null, true);
+    }
+    cb("Solo se permiten archivos de imagen (JPEG/JPG/PNG)");
   }
 };
 var upload = multer(multerConfig);
--- build/index.js
+++ build/index.js
@@ -38,7 +38,8 @@ app.get("/", function (req, res) {
     description: app.get("pkg").description
   });
 });
-app.use('/uploads', _express["default"]["static"](_path["default"].join(__dirname, '../public/uploads')));
+var publicDir = _path["default"].join(__dirname, 'public');
+app.use(_express["default"]["static"](publicDir));
 app.use("/api/products", _productsRoutes["default"]);
 app.use("/api/auth", _authRoutes["default"]);
 app.use("/api/sliders", _slidersRoutes["default"]);
