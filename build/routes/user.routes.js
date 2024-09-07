c979b09874df8dd53be7927b9b4c318947b183f4
Jhonatan ardila <jhonatan.alvarez@netuxtecnologia.com>
2024-09-07 11:31:10 -0500

    feat: change valid route users admin

--- build/routes/user.routes.js
+++ build/routes/user.routes.js
@@ -12,7 +12,7 @@ function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return 
 function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != _typeof(e) && "function" != typeof e) return { "default": e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n["default"] = e, t && t.set(e, n), n; }
 var router = (0, _express.Router)();
 router.post("/", [_middlewares.verifySignup.checkRolesExisted, _middlewares.verifySignup.checkDuplicateUsernameOrEmail], userCtrl.createUser);
-router.get("/", [_middlewares.authJwt.verifyToken, _middlewares.authJwt.isModerator], userCtrl.getUsers);
+router.get("/", [_middlewares.authJwt.verifyToken, _middlewares.authJwt.isAdmin], userCtrl.getUsers);
 router.get("/:userId", userCtrl.getUserById);
 router.put("/:userId", _middlewares.verifySignup.checkRolesExisted, userCtrl.updateUserById);
 router["delete"]("/:userId", userCtrl.deleteUserById);
